94524db987cf01205c7332076cf35ab03797e5e5
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-04-03 16:57:41 -0700

    fixed grid

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ frame1.grid(column=0, row=0)
 frame2 = tk.Frame(window)
 frame2.grid(column=1, row=0)
 
+text_area = tk.Text(master=frame1, height=12, width=30, padx=10, pady=10, bg="#CAD5E2")
+text_area.grid(column=0, row=0)
+text_area.insert(tk.END, "\n\nYour Score : 0 \nComputer Score : 0 \n \nClick on any button to start.")
+
 window.mainloop()
