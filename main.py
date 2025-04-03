2bf3c61c64f607751943086043dbc8ef04516c85
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-04-03 16:53:00 -0700

    added frame

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ window = tk.Tk()
 window.geometry("400x300")
 window.title("Rock Paper Scissors Game")
 
+frame1 = tk.Frame(window)
+frame1.grid(column=0, row=0)
+
+frame2 = tk.Frame(window)
+frame2.grid(column=1, row=0)
+
 window.mainloop()
