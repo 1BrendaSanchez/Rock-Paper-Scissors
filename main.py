4282a96b872827e5b7961375f5321b7aad8afc36
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-04-03 17:05:37 -0700

    designed second frame

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 window.geometry("400x300")
 window.title("Rock Paper Scissors Game")
 
-frame1 = tk.Frame(window)
+frame1 = tk.Frame(window, padx=5, pady=5)
 frame1.grid(column=0, row=0)
 
 frame2 = tk.Frame(window)
@@ -13,5 +13,12 @@ frame2.grid(column=1, row=0)
 text_area = tk.Text(master=frame1, height=12, width=30, padx=10, pady=10, bg="#CAD5E2")
 text_area.grid(column=0, row=0)
 text_area.insert(tk.END, "\n\nYour Score : 0 \nComputer Score : 0 \n \nClick on any button to start.")
+
+button1 = tk.Button(frame2, text="      Rock     ", bg="#50DBB4", padx=20, pady=25)
+button1.grid(column=0, row=0)
+button2 = tk.Button(frame2, text="     Paper    ", bg="#50DBB4", padx=20, pady=25)
+button2.grid(column=0, row=1)
+button3 = tk.Button(frame2, text="   Scissors  ", bg="#50DBB4", padx=20, pady=25)
+button3.grid(column=0, row=2)
 
 window.mainloop()
